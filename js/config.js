a71d01312773d708ffcec7933ec48c0c23f05016
zingoldcow-ship-it <zingoldcow@gmail.com>
2026-02-05 16:20:24 +0900

    Update SUBMIT_ENDPOINT URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbx851thgkhhl92LVCpcrVJJw-ek8c5Y3Eeh1oSc99XTPnRerfO60xX6Z0L8Lr_Cv0QCBg/exec";
+window.SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbzdBexBQglNQ6VpdQbEB-C-nmJPnrKtW778oDm86UXY6VkSi9uL9ueGaLP2ekELbh_7iQ/exec";
