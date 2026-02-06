a0dca2b400bdd81a10f8c872a1cd158017fce8ed
zingoldcow-ship-it <zingoldcow@gmail.com>
2026-02-06 10:57:54 +0900

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-window.SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbw-JJ0CuGIw0lgaHpbIHhQTSjDhB2buqNGkvx2sYuAyZv5QRpieP3JH9ItvgPh-V7ekZA/exec";
+window.SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbzdBexBQglNQ6VpdQbEB-C-nmJPnrKtW778oDm86UXY6VkSi9uL9ueGaLP2ekELbh_7iQ/exec";
